4db261d443cc7bac39554614060fb82652aa00e5
Sebastian Boettcher <seb.boett@gmail.com>
2016-10-07 16:07:59 +0200

    removed a bunch of unnecessary stuff

--- platypus/firmware/install.sh
+++ platypus/firmware/install.sh
@@ -26,41 +26,16 @@ echo
 echo "> Testing platypus"
 bin/platypusTest --id-only --config platypus.conf
 
-# only continue if tests successful
-#RETVAL=$?
-#[ $RETVAL -ne 0 ] && exit
-
 echo
 echo "> Copying some relevant files"
 mkdir -p /etc/platypus
-cp bin/platypusMain /usr/bin/platypus
 cp bin/platypusTest /usr/bin/platypus-test
 
-cp platypus.service /lib/systemd/system/
 cp platypus-test.service /lib/systemd/system/
-cp syslog.service /lib/systemd/system/
 
 cp platypus.conf /etc/platypus/
-cp init_i2c8.sh /etc/platypus/
-cp init_mcu_spi.sh /etc/platypus/
-cp syslog.sh /etc/platypus/
-
-cp mcu/intel_mcu.bin /lib/firmware/
 
 systemctl daemon-reload
-
-echo
-echo "> one time mcu PWM setup"
-./init_mcu_PWM.sh
-
-echo
-echo "> enabling autostart services, platypus will automatically start on next reboot"
-systemctl enable platypus
-systemctl enable syslog
-
-echo
-echo "> you need to reboot to enable the installed mcu script!"
-echo "> PLEASE MAKE SURE THE IMU INTERRUPT IS CONNECTED ON THE BOARD!"
 
 echo
 while true; do
